14da59d24ab40ec746f2d0ea9149f2aa8bc6d480
Mateusz Kiszka <mateusz.kiszka@komlogo.pl>
2022-06-27 07:49:01 +0200

    Created MyPrettyCanvas to draw on it.
    Rectangle animation.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class MyPrettyCanvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+    this.time = 0;
+  }
+
+  draw() {
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = this.canvas.current.getContext("2d");
+    let { size, rectanglesCount } = this.props;
+    ctx.clearRect(0, 0, size, size);
+    ctx.fillStyle = "rgba(0,150,0,0.2)";
+    ctx.save();
+    ctx.translate(size / 2, size / 2);
+    //    console.log(size);
+    // minuta: 6:06
+    const maxRectSize = size / Math.sqrt(2);
+    for (let index = 0; index < rectanglesCount; index++) {
+      const rectSize = maxRectSize * (index / (rectanglesCount));
+      const rectAngle = (index / (rectanglesCount));
+
+      ctx.save();
+      ctx.rotate(rectAngle * this.time);
+      ctx.translate(-rectSize / 2, -rectSize / 2);
+      ctx.fillRect(0, 0, rectSize, rectSize);
+      ctx.restore();
+    }
+    ctx.restore();
+
+  }
+
+  componentDidMount() {
+    this.animationRequestedId = window.requestAnimationFrame(this.update);
+  }
+
+  componentDidUpdate() {
+    
+  }
+
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this.animationRequestedId);
+  }
+
+  update = () => {
+    this.time += 0.02;
+    this.draw();
+    console.log(this.time);
+    this.animationRequestedId = window.requestAnimationFrame(this.update);
+  }
+
+  render() {
+    const { size } = this.props;
+    return <canvas ref={this.canvas} width={size} height={size} style={{ border: "1px solid red" }} />
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =  {reactAmount: 2};
+    
+  }
+  render() {
+    const { reactAmount } = this.state;
+
+    return (
+      <div className="App App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kanwasik ;- ]
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <MyPrettyCanvas size={300} rectanglesCount={reactAmount} />
+        <div style={{ height: "50px" }}>
+          <label>Liczba kwadracików:</label>
+            <input
+              type="number" value={reactAmount} 
+              onChange={(event) => {this.setState({ reactAmount: (event.target.value>=0?event.target.value:0) })}}></input>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
